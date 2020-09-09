f06fa569f16dbfe6f38a13a1996f982a623dd3fc
haliwt <jonehutong@gmail.com>
2020-09-09 15:09:30 +0800

    The USART1 be test is normal ,DATA.2020.09.09

--- usart1.h
+++ usart1.h
@@ -6,11 +6,10 @@
 
 struct senddata{
      uint8_t windlevel ;
-	 uint8_t senddata ;
 	 uint8_t filterNet :1;
 	 uint8_t timerTim :1;
 	 uint8_t sleep :1;
-	 uint8_t childlock :1;
+	 uint8_t childLock:1;
 
 };
 extern struct  senddata ref;
